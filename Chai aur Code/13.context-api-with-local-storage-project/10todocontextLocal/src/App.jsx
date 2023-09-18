d54753c46fb5512cf5f2e95a6a5bf2ac58e68b77
satyam-seth <satyam1998.1998@gmail.com>
2023-09-19 00:45:31 +0530

    add TodoItem and TodoForm component

--- Chai aur Code/13.context-api-with-local-storage-project/10todocontextLocal/src/App.jsx
+++ Chai aur Code/13.context-api-with-local-storage-project/10todocontextLocal/src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import { TodoProvider } from './contexts';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoItem from './components/TodoItem';
+
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -40,10 +43,18 @@ function App() {
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
             <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
             <div className="mb-4">
-                {/* Todo form goes here */} 
+                {/* Todo form goes here */}
+                <TodoForm />
             </div>
             <div className="flex flex-wrap gap-y-3">
                 {/*Loop and Add TodoItem here */}
+                {todos.map((todo) => (
+                  <div key={todo.id}
+                  className='w-full'
+                  >
+                    <TodoItem todo={todo} />
+                  </div>
+                ))}
             </div>
         </div>
       </div>
