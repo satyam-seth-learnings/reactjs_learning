2435ea6d06ab1f5b7a494e5d96169014154ca73e
satyam-seth <satyam1998.1998@gmail.com>
2023-10-20 00:43:16 +0530

    add axios and make api call for products

--- Chai aur Code/19.handle-apis-like-a-pro/frontend/src/App.jsx
+++ Chai aur Code/19.handle-apis-like-a-pro/frontend/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import './App.css';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        // const response = await axios.get('/api/productsd');
+        const response = await axios.get('/api/products');
+        console.log(response.data);
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  if(error) {
+    return <h1>Soothing went wrong</h1>;
+  }
+
+  if(loading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Chai aur API in React</h1>
+      <h2>Number of Products are: {products.length}</h2>
     </>
   )
 }
 
-export default App
+export default App;
