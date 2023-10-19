f70692d0be8073756df478db55f77fe48f66b753
satyam-seth <satyam1998.1998@gmail.com>
2023-10-20 01:11:36 +0530

    use AbortController with axios

--- Chai aur Code/19.handle-apis-like-a-pro/frontend/src/App.jsx
+++ Chai aur Code/19.handle-apis-like-a-pro/frontend/src/App.jsx
@@ -8,35 +8,52 @@ function App() {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
+    const controller = new AbortController();
+
     (async () => {
       try {
         setLoading(true);
         setError(false);
-        // const response = await axios.get('/api/productsd');
-        const response = await axios.get('/api/products');
+        const response = await axios.get('/api/products?search=' + search, {signal: controller.signal});
         console.log(response.data);
         setProducts(response.data);
         setLoading(false);
       } catch (error) {
+        if(axios.isCancel(error)) {
+          console.log('Request Cancel', error.message);
+        }
+
         setError(true);
         setLoading(false);
       }
     })();
-  }, []);
 
-  if(error) {
-    return <h1>Soothing went wrong</h1>;
-  }
+    // Cleanup
+    return () => {
+      controller.abort();
+    };
+  }, [search]);
 
-  if(loading) {
-    return <h1>Loading...</h1>;
-  }
+  // if(error) {
+  //   return <h1>Soothing went wrong</h1>;
+  // }
+
+  // if(loading) {
+  //   return <h1>Loading...</h1>;
+  // }
 
   return (
     <>
       <h1>Chai aur API in React</h1>
+      <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+
+      {loading && (<h1>Loading...</h1>)}
+
+      {error && (<h1>Soothing went wrong</h1>)}
+
       <h2>Number of Products are: {products.length}</h2>
     </>
   )
