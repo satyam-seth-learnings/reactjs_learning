10f3afbc9ed971869a943457ceeae871012b15b4
satyam-seth <satyam1998.1998@gmail.com>
2023-12-12 00:22:02 +0530

    add lifting state up example

--- Geekyshows/Master React JS Complete Basic to Advance/reactjs18comp/ch1/src/App.jsx
+++ Geekyshows/Master React JS Complete Basic to Advance/reactjs18comp/ch1/src/App.jsx
@@ -2,17 +2,21 @@ import { LearnComponent } from "./components/LearnComponent";
 import { LearnEvent } from "./components/LearnEvent";
 import { LearnJSX } from "./components/LearnJsx";
 import { LearnProps } from "./components/LearnProps";
+import {LearnLiftingStateUp} from "./components/LearnLiftingStateUp";
 
 function App() {  
   let roll = 101
-
+  const getData = (data) => {
+    console.log(data);
+  }
 
   return (
     <>
       {/* <LearnComponent /> */}
       {/* <LearnJSX /> */}
       {/* <LearnProps name="Satyam Seth" roll={roll} /> */}
-      <LearnEvent />
+      {/* <LearnEvent /> */}
+      <LearnLiftingStateUp myClick={getData} />
     </>
   )
 }
